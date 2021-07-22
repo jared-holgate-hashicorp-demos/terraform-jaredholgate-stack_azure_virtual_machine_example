0dcdc3e5934f111bbfd0a54586111b58ed29df99
Jared Holgate <jaredfholgate@gmail.com>
2021-07-22 11:05:39 +0100

    Try sudo

--- vault.bash
+++ vault.bash
@@ -116,10 +116,12 @@ RestartSec=42s
 WantedBy=multi-user.target
 EOF
 
-systemctl daemon-reload
+sudo systemctl daemon-reload
 
 sudo systemctl start vault
 
 sudo systemctl status vault
 
-vault operator init -address="http://127.0.0.1:8200" -format=json > /opt/vault/init.log 2>&1
+sudo vault status
+
+sudo vault operator init -address="http://127.0.0.1:8200" -format="json" > /opt/vault/init.log 2>&1
