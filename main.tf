ed2f4aa14840c690f79b99f4a4395ca6c88187da
Jared Holgate <jaredfholgate@gmail.com>
2021-07-22 01:07:57 +0100

    Fix length

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ data "azurerm_client_config" "current" {
 }
 
 resource "azurerm_key_vault" "vault" {
-  name                = "jared-holgate-hashicorp-vault-keyvault-${var.environment}"
+  name                = "jared-holgate-hashi-${var.environment}"
   location            = var.location
   resource_group_name = var.resource_group_name
   tenant_id           = data.azurerm_client_config.current.tenant_id
